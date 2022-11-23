25ff8f3ed6521762b417bdd814d05cfe44bddd75
Mirage Kamran <mirage.kamran@outlook.com>
2022-11-23 14:11:23 -0500

    unused codes deleted

--- 8Ball/ViewController.swift
+++ 8Ball/ViewController.swift
@@ -10,14 +10,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var ballImageView: UIImageView!
-    
-//    var ballNumber = 1
-
 
     @IBAction func askButton(_ sender: Any) {
+        
         let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5",]
         ballImageView.image = UIImage(named: ballArray[Int.random(in: 0...4)])
-        
         
     }
 }
